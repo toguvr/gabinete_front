f50a7d28528031a46987921b233d180fb132700e
unknown <otaviochrispim@gmail.com>
2023-02-10 10:46:59 -0300

    criando service e authcontext

--- src/dtos/index.ts
+++ src/dtos/index.ts
@@ -1,3 +1,35 @@
 export interface StateProps {
   [key: string]: any;
 }
+
+export interface User {
+  id: string;
+  name: string;
+  cellphone: string;
+  logo_url: string;
+  avatar: string;
+  avatar_url: string;
+  verified: boolean;
+  role: string;
+  email: string;
+  cpf: string;
+  rg: string;
+  crm: string;
+  sus: string;
+  bank: string;
+  agency: string;
+  account: string;
+  street: string;
+  complemento: string;
+  number: string;
+  cep: string;
+  bairro: string;
+  cidade: string;
+  expoToken: string;
+  uf: string;
+  notifyConfirmation: boolean;
+  notifyOffer: boolean;
+  notifyAlert: boolean;
+  created_at: Date;
+  updated_at: Date;
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Demanda from "../pages/Demanda";
 import Eleitor from "../pages/Eleitor";
 import Equipe from "../pages/Equipe";
@@ -7,11 +7,19 @@ import Signin from "../pages/SignIn";
 import Tarefa from "../pages/Tarefa";
 
 export default function AppRoutes() {
+  // const ProtectedRoute = ({ user, redirectPath = "/landing", children }) => {
+  //   if (!user) {
+  //     return <Navigate to={redirectPath} replace />;
+  //   }
+
+  //   return children;
+  // };
+
   return (
     <Routes>
+      <Route path="/signin" element={<Signin />} />
       <Route path="/" element={<Home />} />
       <Route path="/eleitor" element={<Eleitor />} />
-      <Route path="/signin" element={<Signin />} />
       <Route path="/equipe" element={<Equipe />} />
       <Route path="/demanda" element={<Demanda />} />
       <Route path="/tarefa" element={<Tarefa />} />
