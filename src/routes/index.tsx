cd67ee9a549f3c672822a07ff359e9759115038f
Hugo Pereira <hugop868@gmail.com>
2023-02-10 00:54:16 -0300

    subindo

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import Demanda from '../pages/Demanda';
 import Eleitor from '../pages/Eleitor';
 import Equipe from '../pages/Equipe';
 import Home from '../pages/Home';
+import Signin from '../pages/SignIn';
 import Tarefa from '../pages/Tarefa';
 
 export default function AppRoutes() {
@@ -10,6 +11,7 @@ export default function AppRoutes() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/eleitor" element={<Eleitor />} />
+      <Route path="/signin" element={<Signin />} />
       <Route path="/equipe" element={<Equipe />} />
       <Route path="/demanda" element={<Demanda />} />
       <Route path="/tarefa" element={<Tarefa />} />
